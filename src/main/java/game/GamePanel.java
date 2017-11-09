7e964e8a1b01f0a33bd27224b79b04fb37fa5598
MajorPetrov <razdobreevvlad@live.fr>
2017-11-09 21:45:32 +0100

    adding main game loop in GamePanel

--- src/main/java/game/GamePanel.java
+++ src/main/java/game/GamePanel.java
@@ -16,18 +16,91 @@
  */
 package game;
 
-import java.awt.Dimension;
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.JPanel;
 
 /**
  *
  * @author <a href=mailto:razdobreevvlad@yandex.ru> Vladimir Razdobreev </a>
  */
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable, KeyListener {
     private static final int WIDTH = 900;
     private static final int HEIGHT = 550;
+    private static final int FPS = 60;
+    private static final long targetTime = 1000 / FPS;
+    
+    private Thread thread;
+    private boolean isRunning;
     
     public GamePanel() {
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.start();
+    }
+    
+    private void start() {
+        this.isRunning = true;
+        this.thread = new Thread(this);
+        this.thread.start();
+    }
+
+    @Override
+    public void run() {
+        long start, elapsed, wait;
+        
+        while(this.isRunning) {
+            start = System.nanoTime();
+            
+            tick();
+            repaint();
+            
+            elapsed = System.nanoTime() - start;
+            wait = targetTime - elapsed / 1000000;
+            
+            //if if down to 60fps, we need to sleep the thread
+            if(wait <= 0) {
+                wait = 5;
+            }
+            
+            try {
+                thread.sleep(wait); //if it's static -> Thread
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    /**
+     * updating all game logic
+     */
+    public void tick() {
+        System.out.println("running");
+    }
+    
+    /**
+     * to draw graphics
+     * @param g
+     */
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        g.drawRect(10, 10, 10, 10);
+    }
+
+    @Override
+    public void keyTyped(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
